8f4279583d5b4cd31975724c07b45bf23d8298f6
GMMendes01 <g7000mendes@gmail.com>
2024-04-15 19:11:29 -0300

    15_04

---
+++ telas/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Document</title>
+   
+</head>
+<body>
+<?php
+    include_once '../navBar.php';
+    ?>
+    
+    <div class="login-container">
+    <h1>AREA PARA GESTORES</h1>
+     <form action=""></form>   
+    <label for="mesa">Mesa</label>
+    <input type="text" name="mesa" id="mesa_input">
+    <label for="senha">Senha</label>
+    <input type="text" name="senha" id="senha_input">
+    </div>
+</body>
+</html>
