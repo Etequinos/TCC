5712a20abcf61f22c27af4aac17b056c2df7a98f
GMMendes01 <g7000mendes@gmail.com>
2024-04-17 22:14:36 -0300

    a

--- navBar.php
+++ navBar.php
@@ -1,4 +1,3 @@
-
 <head>
     <link rel='stylesheet' href='../css/navbar.css'>
 </head>
---
+++ telas/gestao.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/gestao.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    include_once '../navBar.php';
+    ?>
+    <div class="gestao-container">
+    <h1>LOGIN</h1>
+    <h2>Código</h2>
+    <input type="number" name="codigo" id="codigo">
+    <button type="submit">Confirmar</button>
+    </div>
+</body>
+</html>
