76152e7be8eb34dd98ff381c9b81c3d560a8a748
LucasJoseRocha <lucasjosecampos2@gmail.com>
2024-04-23 19:53:42 -0300

    Junção do backend com a tela da gestão

--- telas/gestao.php
+++ telas/gestao.php
@@ -1,3 +1,33 @@
+<?php
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require 'vendor/autoload.php';
+use PragmaRX\Google2FA\Google2FA;
+$_g2fa = new Google2FA();
+
+
+
+if (!isset($_SESSION['g2fa_user'])) {
+    $user = new stdClass();
+    $user->google2fa_secret = "CRBRT55RJS4DYBZB";
+    $user->email = 'username';
+
+    $_SESSION['g2fa_user'] = $user;
+} else {
+    $user = $_SESSION['g2fa_user'];
+}
+
+$app_name = 'TCC';
+
+$qrCodeUrl = $_g2fa->getQRCodeUrl(
+    $app_name,
+    $user->email,
+    $user->google2fa_secret
+);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,8 +43,24 @@
     <div class="gestao-container">
     <h1>LOGIN</h1>
     <h2>Código</h2>
-    <input type="number" name="codigo" id="codigo">
-    <button type="submit">Confirmar</button>
+    <input type="number" name="otp" id="otp">
+    <button type="button" value="Verify" onclick="verify_otp();" >Confirmar</button>
     </div>
+
+    <script>
+    const verify_otp = async () => {
+        let otp = document.getElementById('otp').value;
+        fetch('verify.php?otp=' + otp)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                if (data.result == true) {
+                    alert("Código correto (PASS)");
+                } else{
+                    alert("Código incorreto, tente novamente (DENNY ACCESS)");
+                }
+            });
+    }
+</script>
 </body>
 </html>
