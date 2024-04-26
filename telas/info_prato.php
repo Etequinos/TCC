f5b0f09a3fb6fc96a92a6003abff3bfd75c9cf8b
crocodiles128 <luquinhasjoserocha@gmail.com>
2024-04-26 16:14:19 -0300

    Adicionado Back em Card SQL e info prato

--- telas/info_prato.php
+++ telas/info_prato.php
@@ -17,12 +17,31 @@
 <body>
 <?php
     include_once '../navBar.php';
-
-
-
-    
+    include '../conecta.php';    
     ?>
+<?php
+$id = $_GET['id'];
 
+// Consulta os dados do prato com o ID fornecido
+$sql = "SELECT * FROM Pratos WHERE ID = $id";
+$result = $conexao->query($sql);
+
+if ($result->num_rows > 0) {
+    // Exibe os detalhes do prato
+    while($row = $result->fetch_assoc()) {
+        echo "<h2>" . $row["Nome"] . "</h2>";
+        echo "<p>Descrição: " . $row["Descricao"] . "</p>";
+        echo "<p>Valor: R$ " . $row["Valor"] . "</p>";
+        echo "<p>Valor com Desconto: " . $row["Fidelidade"] . "</p>";
+        echo "<img src='" . $row["Imagem"] . "' alt='" . $row["Nome"] . "'>";
+        echo "<p>Ingredientes: " . $row["Ingredientes"] . "</p>";
+        // Exiba outras informações conforme necessário
+   
+    }
+} else {
+    echo "Prato não encontrado";
+}
+?>
 
 </body>
 </html>
