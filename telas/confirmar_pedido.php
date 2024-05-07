0df3206568155e8d5955bd234548839d1536baf0
GMMendes01 <g7000mendes@gmail.com>
2024-05-06 22:07:18 -0300

    resumo pedido

--- telas/confirmar_pedido.php
+++ telas/confirmar_pedido.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/confirmarpedido.css">
     <title>Document</title>
 
    
@@ -17,5 +17,30 @@
     
     ?>
 
+<div class="container">
+<div class="titulo">
+<h1>RESUMO PEDIDO</h1>
+<button name="finalizar">FINALIZAR</button>
+</div>
+<div class="tabela">
+    <table border="1">
+    <colgroup>
+    <col style="width: 15%;">
+    <col style="width: 55%;">
+    <col style="width: 30%;">
+  </colgroup>
+        <tr>
+    <th>QNT</th>
+    <th>ITEM</th>
+    <th>VALOR TOTAL</th>
+        </tr>
+        <tr >
+        <td>$qnt</td>
+        <td>$nomeprato</td>
+        <td>$99,99</td>
+        </tr>
+    </table>
+</div>
+</div>
 </body>
 </html>
