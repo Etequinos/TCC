d46db50d57adf770c658a1208c7e90442424eb2a
GMMendes01 <g7000mendes@gmail.com>
2024-05-07 21:59:34 -0300

    pagamento

--- telas/multiplo_pagamento.php
+++ telas/multiplo_pagamento.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/mult_pagamento.css">
     <title>Document</title>
 
    
@@ -16,6 +16,37 @@
 
     
     ?>
+    <div class="pagamento">
+        <div class="titulo">
+<h1>COMPARTILHAR PAGAMENTO</h1>
+</div>
+<div class="conteudo">
+    <div class="valorttl">
+        <label for="" class="valores">VALOR TOTAL</label>
+        <label for="" name="valor">$valor</label>
+    </div>
 
+    <div class="numero">
+<label for="" class="valores">NÚMERO DE PESSOAS</label>
+    <input type="number" name="pessoas" id="">
+
+    </div>
+    <div class="valorpessoa">
+<label for="" class="valores">VALOR POR PESSOA</label>
+<label for="" name="dividido">$dividido</label>
+</div>
+<label for="" class="valores">FORMAS DE PAGAMENTO</label>
+</div>
+<div class="imagem">
+<img src="../imagens/download.png" alt="">
+</div>
+</div>
+    </div>
+    <div class="botoes">
+    <button id="add" type="button" href="onboarding.php">CANCELAR</button>
+    <form action="pagamento">
+    <button id="add" type="submit"> CONFIRMAR PAGAMENTO</button>
+    </form>
+</div>
 </body>
 </html>
--- telas/pagamento_QrCode.php
+++ telas/pagamento_QrCode.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/qrcode.css">
     <title>Document</title>
 
    
@@ -16,6 +16,11 @@
 
     
     ?>
+    <div class="titulo">
+<h1>COMPARTILHAR PAGAMENTO</h1>
+</div>
+<div class="pagamento">
+    </div>
 
 </body>
 </html>
