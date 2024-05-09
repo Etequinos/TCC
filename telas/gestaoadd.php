66af2e885ca78220f9cd78b29ca6523fa31f0f30
GMMendes01 <g7000mendes@gmail.com>
2024-05-08 22:11:48 -0300

    muitas mudanças

---
+++ telas/gestaoadd.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/gestaoadd.css">
+    <title>Document</title>
+   
+</head>
+<body>
+<?php
+    include_once '../navBar.php';
+    ?>
+<div class="container">
+    <div class="titulo">
+        <label for="" name="titulo">Descrição do Prato</label>
+    </div>
+    <div class="atributos-prato">
+    <label for="">Nome</label>
+    <form action="">
+        <input type="text">
+    </form>
+    <label for="">Descrição</label>
+    <form action="">
+        <input type="text">
+    </form>
+    <label for="">Valor</label>
+    <form action="">
+        <input type="number">
+    </form>
+    <label for="">Valor desconto</label>
+    <form action="">
+        <input type="number">
+    </form>
+    <label for="">Link Img</label>
+    <form action="">
+        <input type="text">
+    </form>
+    <label for="">Ingredientes</label>
+    <form action="">
+        <input type="text">
+    </form>
+    </div>
+</div>
+
+
+
+</body>
+</html>
--- telas/pagamento_maquina.php
+++ telas/pagamento_maquina.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/maquina.css">
     <title>Document</title>
 
    
@@ -16,6 +16,18 @@
 
     
     ?>
-    
+    <div class="pagamento">
+    <div class="titulo">
+<h1>PAGAR COM CARTÃO</h1>
+    </div>
+    <div class="conteudoMAQUINA">
+    <LAbel>AGUARDE ENQUANTO LEVAMOS A MAQUINA ATÉ SUA MESA</LAbel>
+    <div class="valor"> 
+            <label for="" name="total">VALOR TOTAL</label>
+            <label for="" name="valor">$valor</label>
+        </div>
+</div>    
+        
+</div>
 </body>
 </html>
