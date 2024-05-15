c820997ed3bfe04e0da313ac26c28be66db4f056
GMMendes01 <g7000mendes@gmail.com>
2024-05-15 20:17:37 -0300

    po

--- telas/gestaoadd.php
+++ telas/gestaoadd.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/gestaoadd.css">
     <title>Document</title>
-   
+    
 </head>
 <body>
 <?php
