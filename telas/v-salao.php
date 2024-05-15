21acefb5adf73b8360d3631ee0cbcd22a9e3fac6
GMMendes01 <g7000mendes@gmail.com>
2024-05-15 16:58:04 -0300

    muitas mudanças

--- telas/v-salao.php
+++ telas/v-salao.php
@@ -1,13 +1,27 @@
 <head>
-<link rel='stylesheet' href='../css/card-v-salao.css'>
+<link rel='stylesheet' href='../css/v-salao.css'>
 </head>
 <body>  
+<?php
+include_once '../navBar.php';
 
+include '../conecta.php';
+?>
 
     <div class="content">
         <div class="card-container">
-            <div class="card-name-v">
-                <label for="prato" name="mesa">$Mesa</label>
+                <div class="card-name-v">
+                    <label for="prato" name="mesa">$Mesa</label>
+                    <input type="checkbox" id="cbx2" style="display: none;">
+                    <label for="cbx2" class="check">
+                    <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                    </svg>
+                    </label>
+                    <div class="container">
+                   
+                </div>
             </div>
             <div>
             <p>Quant</p>  
