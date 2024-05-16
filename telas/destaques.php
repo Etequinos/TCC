48f5ea4bec3e49431a6025dc3c9e92218d95b539
MGabriel011 <gabrielchiarella10@gmail.com>
2024-05-15 22:11:56 -0300

    Destaques da semana e outras pequenas alterações.

--- telas/destaques.php
+++ telas/destaques.php
@@ -3,16 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/destaques.css">
     <title>Document</title>
 
 </head>
 <body>
     
-    <h1>Destaques da Semana</h1>
-    
 <?php //Fiz um codigo gigante, não funcionou, dessisti ?>
 
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+    <div class="texto">
+        <h1>PRATOS EM DESTAQUE</h1>
+    </div>
+
+    <div class="valor">
+        <h1>$VALOR FIDELIDADE</h1>
+    </div>
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <img src="../imagens/canva.png" style="width: 100%;">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="../imagens/pao.jpg" style="width:100%;">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="../imagens/coca.jpg" style="width:100%;">
+  </div>
+</div>
+
+<script>
+    let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 </script>
 
 </body>
