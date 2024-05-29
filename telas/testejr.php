51425525dc88880a9644bd85e1a268c9e6fcc169
GMMendes01 <g7000mendes@gmail.com>
2024-05-28 22:20:16 -0300

    gifs

--- telas/testejr.php
+++ telas/testejr.php
@@ -8,6 +8,6 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);"scrolling="yes" width="100%" height="100vh" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FPuwGkLGHs5b89nOKja57V0%2FUntitled-(Copy)%3Fnode-id%3D263-4%26t%3D3DYR8uKUw7Lwf5Sf-1%26scaling%3Dmin-zoom%26starting-point-node-id%3D263%253A4" allowfullscreen></iframe>
+    <img src="test1.gif" alt="teste" width="300px" height="400px">
 </body>
 </html>
