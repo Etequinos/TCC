6d47d1b90fe9cc4907d79a8c5a79baa6bfc71b18
LucasJoseRocha <lucasjosecampos2@gmail.com>
2024-06-05 22:15:32 -0300

    continuarei dps, v-salao para pagar conta

--- telas/chamar-pagamento.php
+++ telas/chamar-pagamento.php
@@ -1,21 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
-    <title>Document</title>
-
-   
-</head>
-<body>
 <?php
-    include_once '../navBar.php';
+include '../conecta.php';
 
 
+$id = $_GET["id"];
 
+$sql = "SELECT * FROM pratos WHERE ID = $id";
+$resultado = mysqli_query($conexao, $sql);
+if ($resultado->num_rows > 0) {
+    $row = $resultado->fetch_assoc();
+$prato_ID = $row["ID"];
+session_start();
+
+$mesa_ID = $_SESSION['ID'];
+$sql = "INSERT INTO pedidos (ID, ID_Mesa, ID_Prato, status) VALUES (NULL, '$mesa_ID', '$prato_ID', 0);";
+$resultado = mysqli_query($conexao, $sql);
+}
     
     ?>
-
-</body>
-</html>
